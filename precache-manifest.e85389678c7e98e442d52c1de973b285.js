6365f0232eb0f2abb23ee5d9f495847ddb8af691
DajanaS <dajana.sk@hotmail.com>
2019-01-25 11:46:20 +0100

    Updates

---
+++ precache-manifest.e85389678c7e98e442d52c1de973b285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a673e608a6ee303308b65042399a12e",
+    "url": "/Static-Content-Website/static/media/background.1a673e60.png"
+  },
+  {
+    "revision": "a71a8e79e160cc6fe099dc06e8e5cdd9",
+    "url": "/Static-Content-Website/static/media/arrow.a71a8e79.png"
+  },
+  {
+    "revision": "7e2d34ae60746cecd0dd",
+    "url": "/Static-Content-Website/static/js/runtime~main.7e2d34ae.js"
+  },
+  {
+    "revision": "96c984a0b747371796f9",
+    "url": "/Static-Content-Website/static/js/main.96c984a0.chunk.js"
+  },
+  {
+    "revision": "2021683b3e8f319ee96b",
+    "url": "/Static-Content-Website/static/js/1.2021683b.chunk.js"
+  },
+  {
+    "revision": "96c984a0b747371796f9",
+    "url": "/Static-Content-Website/static/css/main.56cfc6de.chunk.css"
+  },
+  {
+    "revision": "b5aa4b924ebdb49170e61dc3c16e6a35",
+    "url": "/Static-Content-Website/index.html"
+  }
+];
